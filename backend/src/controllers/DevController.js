5e7263aa26f642168f2472e7fb9c632f21592412
Diogo Cardoso <diogomescardoso@gmail.com>
2019-08-07 12:31:07 -0300

    Busca na api do github com axios

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -6,11 +6,9 @@ const axios = require('axios');
 module.exports = {
    async store(req,res){
       const {username} = req.body;//selecionando apenas 'username'
-      try {
-         const response = await axios.get(`https://api.github.com/users/${username}`);
-      } catch (error) {
-         console.log('Erro no get');
-      }
+      
+      const response = await axios.get(`https://api.github.com/users/${username}`);
+      console.log(`Buscando ${username} no Github` );
       return res.json(response.data);
    }
 };
