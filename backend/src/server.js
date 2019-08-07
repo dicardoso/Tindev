2efff84ad9b94a70e712f7d8d56c0c241beacb4e
Diogo Cardoso <diogomescardoso@gmail.com>
2019-08-06 23:41:02 -0300

    Conexões básicas do servidor

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,8 @@
 const express = require('express');//importando biblioteca Express
+const routes = require('./routes');
 
 const server = express();//cria servidor Express
 
-/*Criando rota raiz
-* req -> traz todas informações referentes à requisição do usuário
-* res -> objeto para retornar resposta ao usuário
-*/
-server.get('/', (req,res) =>{ 
-   return res.json({ message: `Olá ${req.query.name}, tudo em cima?`});
-   //USANDO CRASE (template string)
-   //retorna todos os parametros que envia na url
-   //json necessita de {} para enviar uma mensagem 
-});
-server.listen(3333);//habilitando porta 3333 para ouvir requisissões 
+server.use(express.json());
+server.use(routes);//adicionando rotas
+server.listen(5555);//habilitando porta 3333 para ouvir requisissões 
