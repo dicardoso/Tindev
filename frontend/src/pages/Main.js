ecdf2651797997c1b3f962b8fb8f5c78e9d61697
dicardoso <diogomescardoso@gmail.com>
2019-10-14 23:45:15 -0300

    Acesso da api e populando a tela de devs

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,67 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+
+import api from '../services/api.js';
+
 import './Main.css';
 
 export default function Main({match}){//match possui todos os parâmetros passados para essa rota
+    const [users, setUsers] = useState([]);
+    
+    useEffect(()=> {
+        async function loadUsers(){
+            const response = await api.get('/devs', {
+                headers:{
+                    user: match.params.id //envia para rota devs o id logado
+                }
+            })
+            setUsers(response.data);//seta o estado
+        }
+
+        loadUsers();
+    },[match.params.id])
+    
     return(
         <div className="main-container">
             <img src={logo} alt="Tindev"></img>
             <ul>
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/32266873?v=4" alt=""/>
-                    <footer>
-                        <strong>Diogo Cardoso</strong>
-                        <p>Estudante do curso Superior de Sistemas para Internet no IFPB. Apaixonado por desenvolvimento de games e web.</p>
-                    </footer>
+                {users.map(user => (
+                    <li key={user._id}>
+                        <img src={user.avatar} alt={user.avatar}/>
+                        <footer>
+                            <strong>{user.name}</strong>
+                            <p>{user.bio}</p>
+                        </footer>
 
-                    <div className="button">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike"/>
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="Like"/>
-                        </button>
-                        
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/32266873?v=4" alt=""/>
-                    <footer>
-                        <strong>Diogo Cardoso</strong>
-                        <p>Estudante do curso Superior de Sistemas para Internet no IFPB. Apaixonado por desenvolvimento de games e web.</p>
-                    </footer>
-
-                    <div className="button">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike"/>
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="Like"/>
-                        </button>
-                        
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/32266873?v=4" alt=""/>
-                    <footer>
-                        <strong>Diogo Cardoso</strong>
-                        <p>Estudante do curso Superior de Sistemas para Internet no IFPB. Apaixonado por desenvolvimento de games e web.</p>
-                    </footer>
-
-                    <div className="button">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike"/>
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="Like"/>
-                        </button>
-                        
-                    </div>
-                </li>
+                        <div className="buttons">
+                            <button type="button">
+                                <img src={dislike} alt="Dislike"/>
+                            </button>
+                            <button type="button">
+                                <img src={like} alt="Like"/>
+                            </button>
+                        </div>
+                    </li>
+                ))}
             </ul>
         </div>
     );
