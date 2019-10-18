c8a33688a1b37a89485b323dff14ef5a0955b1c7
dicardoso <diogomescardoso@gmail.com>
2019-10-17 22:44:15 -0300

    Finalizando mobile

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -8,7 +8,7 @@ import logo from '../assets/logo.png';
 
 export default function Login({ navigation }){
     const [user, setUser] = useState('');
-    useEffect(()=>{
+    useEffect(()=>{//mantendo usuario logado
         AsyncStorage.getItem('user').then(user => {
             if(user){
                 navigation.navigate('Main', { user })
@@ -20,11 +20,11 @@ export default function Login({ navigation }){
         const response = await api.post('/devs', {username: user});
 
         const { _id } = response.data;
+        console.log(_id);
 
         await AsyncStorage.setItem('user', _id);
 
-        navigation.navigate('Main', { _id });
-        
+        navigation.navigate('Main', { user: _id });    
     }
 
     return (
